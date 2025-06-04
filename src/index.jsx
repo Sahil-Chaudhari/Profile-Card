2c710c371fefba1eefcdafa597aa9482dc34eefe
Sahil Chaudhari <chaudharisahil475@gmail.com>
2025-06-04 15:33:17 +0530

    Updated publication section in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -114,9 +114,7 @@ function App() {
     <div className="profile-container">
       <h1 align="center">Hi 👋, I'm Sahil Chaudhari</h1>
       <h3 align="center" font-weight=" 400" >
-        I'm an IT graduate from Vishwakarma Institute of Information Technology,
-        Pune. I'm a Tech Enthusiast. I love to explore and learn about new
-        domains and technologies.
+        Motivated Btech graduate in Information Technology from VIIT, Pune with practical experience in MERN stack development, Data Science, and Machine Learning. Looking to work in a healthy, innovative, and challenging environment that fosters continuous learning and personal as well as professional growth.
       </h3>
 
       <div style={{ textAlign: "center", margin: "20px 0" }}>
@@ -144,7 +142,7 @@ function App() {
         <li>📫 Reach me at: <strong>chaudharisahil475@gmail.com</strong></li>
       </ul>
 
-      <h3>Connect with me:</h3>
+      <h3>CONNECT WITH ME:</h3>
       <p>
         <a href="https://twitter.com/sahil09989938" target="_blank" rel="noopener noreferrer">
           <img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="Twitter" height="30" width="40" />
@@ -163,7 +161,7 @@ function App() {
 </a>
       </p>
 
-      <h3>Languages and Tools:</h3>
+      <h3>LANGUAGES AND TOOLS:</h3>
       <div className="tools">
         {[
           "cplusplus",
@@ -189,7 +187,7 @@ function App() {
           />
         ))}
       </div>
-      <h3>Work Experience:</h3>
+      <h3>WORK EXPERIENCE:</h3>
 <div className="experience">
   <div className="experience-item">
     <h4>Data Science Engineer @ Piyush IT Services (Consulting startup)</h4>
@@ -209,20 +207,20 @@ function App() {
     </ul>
   </div>
 </div>
-<h3>Projects:</h3>
+<h3>PROJECTS:</h3>
 <div className="projects">
   <div className="project-item">
     <h4>
-      <a
+      
+        Multiple Disease Prediction System <a
         href="https://github.com/Sahil-Chaudhari/Multiple-Disease-Prediction"
         target="_blank"
         rel="noopener noreferrer"
         style={{ filter: "grayscale(100%) brightness(0%)" }}
-      >
-        Multiple Disease Prediction System
-      </a>
+      >🔗</a>
+      
     </h4>
-    <p><strong>Oct 2024</strong></p>
+    <p><strong>October 2024</strong></p>
     <ul>
       <li>Web application for disease prediction.</li>
       <li>
@@ -238,14 +236,14 @@ function App() {
 
   <div className="project-item">
     <h4>
-      <a
+      
+        Modern Real Estate Marketplace<a
         href="https://github.com/Sahil-Chaudhari/Modern-Real-Estate-Marketplace"
         target="_blank"
         rel="noopener noreferrer"
         style={{ filter: "grayscale(100%) brightness(0%)" }}
-      >
-        Modern Real Estate Marketplace
-      </a>
+      >🔗</a>
+      
     </h4>
     <p><strong>May 2024</strong></p>
     <ul>
@@ -260,6 +258,175 @@ function App() {
       </li>
     </ul>
   </div>
+    <div className="project-item">
+    <h4>
+      
+        Attendance System using Facial Recognition
+      <a
+        href="https://github.com/Sahil-Chaudhari/Attendance-System-using-Face-Recognition-in-Python"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+    </h4>
+    <p><strong>January 2024</strong></p>
+    <ul>
+      <li>A Full-proof system which mark our attendance using <strong>Facial-recognition, OpenCV and dlib</strong> in Python</li>
+    </ul>
+  </div>
+    <div className="project-item">
+    <h4>
+      
+        Digi-Diary
+      <a
+        href="https://github.com/Pdeo55/Digi-diary"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+    </h4>
+    <p><strong>December 2023</strong></p>
+    <ul>
+      <li>A Full-Stack application to digitize daily Diary-Management for primary school students, facilating better communication between teachers and parents.</li>
+      <li>Implemented functionalities for teachers to manage student records, assign homework with media attachments,post public notices using technologies such as <strong>React.js, Redux, Express.js, Node.js, MongoDB.</strong>
+      </li>
+      <li>Created a user-friendly interface allowing parents to view Homework, access notices, raise queries and set reminders for homework through Whatsapp-notifications.
+      </li>
+      <li>Utilized <strong>AWS S3 Bucket</strong> for file storage and <strong>AWS EC2</strong> for deployment, integrated <strong>NodeMailer and Cloudinary </strong>for Email and media-management.</li>
+    </ul>
+  </div>
+    <div className="project-item">
+    <h4>
+      
+        ATM Simulator
+      <a
+        href="https://github.com/Sahil-Chaudhari/ATM-Simmulator-System"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+    </h4>
+    <p><strong>October 2023</strong></p>
+    <ul>
+      <li>Java based project named ATM Simulator System developed using <strong>Java(Swing), MySQL</strong> which has functionalities like Opening Banking Account, Deposit, Withdraw, Mini-Statement, Pin Change, etc.</li>
+      
+    </ul>
+  </div>
+    <div className="project-item">
+    <h4>
+      
+        Book-it (Event Booking Services App)
+      <a
+        href="https://github.com/Pdeo55/BookIT-Frontend"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+    </h4>
+    <p><strong>May 2023</strong></p>
+    <ul>
+      <li>Designed and developed a full-stack event booking platform enabling users to browse, book, and manage event tickets. Implemented real-time seat availability, secure user authentication, and an admin dashboard for event management using <strong>React.js, Node.js, Express.js, MongoDB</strong></li>
+      
+    </ul>
+  </div>
+    <div className="project-item">
+    <h4>
+      
+        HR-Attrition Dashboard
+      <a
+        href="https://github.com/Sahil-Chaudhari/HR-Attrition-Dashboard"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+    </h4>
+    <p><strong>May 2023</strong></p>
+    <ul>
+      <li>Developed an HR Attrition Analysis dashboard using <strong>Power BI</strong> to visualize workforce data and identify attrition trends.</li>
+<li>Utilized Power Query to clean, transform, and prepare raw CSV data for accurate analysis.</li>
+<li>Created interactive reports and visualizations for deeper insights into employee attrition factors.</li>
+    </ul>
+  </div>
+  <h3>PUBLICATIONS:</h3>
+<div className="projects">
+  <div className="project-item">
+    <h4>
+      
+        Research Paper
+      
+    </h4>
+    
+    <ul>
+      <li>Published reserch paper on "Multiple Disease Prediction using Machine Learning Algorithms" at ICCUBEA 2023.
+        <a
+        href="https://www.linkedin.com/in/sahil-chaudhari-2168aa210/details/certifications/1712575140706/single-media-viewer?type=LINK&profileId=ACoAADWGjrIBQA71WqAnuQ9lmGtpuIjpVO8MhxI&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_certifications_details%3BlTPt1jXNT%2FGOn30e0KiRAw%3D%3D"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+      </li>
+      
+    </ul>
+  </div>
+</div>
+<div className="projects">
+  <div className="project-item">
+    <h4>
+      
+        Copyrights
+      
+    </h4>
+    
+    <ul>
+      <li >Copyright © titled "Book-it (Event Booking Services)" - SW168583/2023. 
+        <a
+        href="https://www.copyright.gov.in/CopyrightROC_Details.aspx?DiaryNo=12288/2023-CO/SW&RocNo=SW-16853/2023"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+      </li>
+      
+        <li>
+        Copyright © titled "Digi-diary for primary school students" - L-136032/2023.
+        <a
+        href="https://www.copyright.gov.in/CopyrightROC_Details.aspx?DiaryNo=25569/2022-CO/L&RocNo=L-136032/2023"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      >🔗</a>
+      </li>
+      
+      
+      
+    </ul>
+  </div>
+</div>
+<div className="projects">
+  <div className="project-item">
+    <h4>
+      
+        Book-Chapter
+      
+    </h4>
+    
+    <ul>
+      <li>
+<strong>Chapter named: Enhanced Environmental Monitoring through Blockchain Integration: A Carbon Offset Marketplace Application</strong><br/>
+Beyond Blockchain: Reviewing the Impact and Evolution of Decentralized Networks (Part 1) - Publisher: Bentham Science Publishers - April 1, 2025 
+<a
+        href="https://drive.google.com/file/d/14aSkOMjkk9K9H07yUXVaCOhG48Et-hdz/view?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{ filter: "grayscale(100%) brightness(0%)" }}
+      > 🔗</a>
+</li>
+
+      
+      
+    </ul>
+  </div>
+</div>
 </div>
     </div>
   );
